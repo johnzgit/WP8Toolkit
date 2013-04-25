168ac517dcb9b6dcd3c0dc7efa1e883403feab3d
Ales <ales.daneu@gmail.com>
2013-04-25 11:11:54 +1000

    Moved Storage logic into a separate assembly

---
+++ Ayls.WP8Toolkit.Storage/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ayls.WP8Toolkit.Storage")]
+[assembly: AssemblyDescription("WP8 Toolkit Storage")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ayls Creations")]
+[assembly: AssemblyProduct("Ayls.WP8Toolkit.Storage")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("82006491-f81e-4bab-940a-a8ce25c30226")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
